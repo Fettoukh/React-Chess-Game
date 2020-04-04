e2c1216aff91c024c16ad3cc96706c366958cab8
Fettoukh <medamine.fettoukh@gmail.com>
2020-04-04 17:39:22 +0200

    Updates

---
+++ precache-manifest.bd1c43201f5a9602cf3b6f6e2becbcd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a872fac8e0db26a438b96adeda70a1",
+    "url": "/React-Chess-Game/index.html"
+  },
+  {
+    "revision": "559d57631e4e2f998337",
+    "url": "/React-Chess-Game/static/js/2.c30f9bf4.chunk.js"
+  },
+  {
+    "revision": "a894e60fc32a66120e9738e29556989e",
+    "url": "/React-Chess-Game/static/js/2.c30f9bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edb34b8aa8d81d873bd",
+    "url": "/React-Chess-Game/static/js/main.a353daea.chunk.js"
+  },
+  {
+    "revision": "68c9ec885ea305ea4ff9",
+    "url": "/React-Chess-Game/static/js/runtime-main.dc7c566d.js"
+  }
+]);
